ea967a3366983c1c0512e5e7fd49fdbb3978d5b7
givemelight <github@givemelight.de>
2018-12-04 00:27:48 +0100

    Added original files

---
+++ unb_lib/version.def.php
@@ -0,0 +1,17 @@
+<?php
+// Unclassified NewsBoard
+// Copyright 2003-2015 by Yves Goergen
+// Homepage: http://newsboard.unclassified.de
+// See docs/license.txt for distribution/license details
+//
+// version.def.php
+// Version definition file
+// Don't change this information!
+// Used for online version check and upgrade warning.
+
+$UNB['Version'] = 'unb.devel.20150713';
+$UNB['VersionTime'] = '20150713';
+
+// Last version time that the upgrade script must be run before
+$UNB['VersionUpgrade'] = '20080804';
+?>
